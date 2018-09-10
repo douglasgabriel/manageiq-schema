52d6314b3c05a86e43386938f1871f4e1d085a52
Brandon Dunne <brandondunne@hotmail.com>
2018-09-10 14:23:36 -0400

    rewrite migration and spec

--- db/migrate/20180823111741_move_location_led_state_to_asset_details_table.rb
+++ db/migrate/20180823111741_move_location_led_state_to_asset_details_table.rb
@@ -1,51 +1,39 @@
 class MoveLocationLedStateToAssetDetailsTable < ActiveRecord::Migration[5.0]
   class AssetDetail < ActiveRecord::Base
-    belongs_to :resource, :polymorphic => true
-
-    serialize :location_led_state
-  end
-
-  class PhysicalServer < ActiveRecord::Base
-    self.inheritance_column = :_type_disabled
-
-    has_one :asset_detail, :as => :resource, :dependent => :destroy
   end
 
   class PhysicalChassis < ActiveRecord::Base
     self.inheritance_column = :_type_disabled
+  end
 
-    has_one :asset_detail, :as => :resource, :dependent => :destroy
+  class PhysicalServer < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
   end
 
   def up
     add_column :asset_details, :location_led_state, :string
 
-    PhysicalServer.includes(:asset_detail).each do |server|
-      server.asset_detail ||= server.build_asset_detail
-      server.asset_detail.update_attributes!(:location_led_state => server.location_led_state)
-    end
+    PhysicalChassis.find_each { |chassis| move_location_led_state_to_asset_detail("PhysicalChassis", chassis) }
+    PhysicalServer.find_each { |server| move_location_led_state_to_asset_detail("PhysicalServer", server) }
 
-    PhysicalChassis.all.each do |chassis|
-      chassis.asset_detail ||= chassis.build_asset_detail
-      chassis.asset_detail.update_attributes!(:location_led_state => chassis.location_led_state)
-    end
-
-    remove_column :physical_servers, :location_led_state
     remove_column :physical_chassis, :location_led_state
+    remove_column :physical_servers, :location_led_state
   end
 
   def down
-    add_column :physical_servers, :location_led_state, :string
     add_column :physical_chassis, :location_led_state, :string
+    add_column :physical_servers, :location_led_state, :string
 
-    PhysicalServer.all.each do |server|
-      server.update_attributes!(:location_led_state => server.asset_detail&.location_led_state)
-    end
-
-    PhysicalChassis.all.each do |chassis|
-      chassis.update_attributes!(:location_led_state => chassis.asset_detail&.location_led_state)
+    AssetDetail.where(:resource_type => ["PhysicalChassis", "PhysicalServer"]).find_each do |asset_detail|
+      self.class.const_get(asset_detail.resource_type).update(asset_detail.resource_id, :location_led_state => asset_detail.location_led_state)
     end
 
     remove_column :asset_details, :location_led_state
+  end
+
+  private
+
+  def move_location_led_state_to_asset_detail(class_name, asset)
+    AssetDetail.find_or_initialize_by(:resource_type => class_name, :resource_id => asset.id).update_attributes!(:location_led_state => asset.location_led_state)
   end
 end
--- spec/migrations/20180823111741_move_location_led_state_to_asset_details_table_spec.rb
+++ spec/migrations/20180823111741_move_location_led_state_to_asset_details_table_spec.rb
@@ -1,84 +1,52 @@
 require_migration
 
 describe MoveLocationLedStateToAssetDetailsTable do
-  let(:physical_server_stub)  { migration_stub(:PhysicalServer) }
-  let(:physical_chassis_stub) { migration_stub(:PhysicalChassis) }
   let(:asset_detail_stub)     { migration_stub(:AssetDetail) }
+  let(:physical_chassis_stub) { migration_stub(:PhysicalChassis) }
+  let(:physical_server_stub)  { migration_stub(:PhysicalServer) }
 
   context "resources with asset detail" do
     migration_context :up do
-      it "should migrate the data from physical_chassis and physical_servers to asset_details table" do
-        asset_detail_server = asset_detail_stub.create!
-        physical_server_stub.create!(
-          :asset_detail       => asset_detail_server,
-          :location_led_state => 'Off'
-        )
+      let!(:physical_chassis) { physical_chassis_stub.create!(:location_led_state => 'Blinking') }
+      let!(:physical_server)  { physical_server_stub.create!(:location_led_state => 'Off') }
 
-        asset_detail_chassis = asset_detail_stub.create!
-        physical_chassis_stub.create!(
-          :asset_detail       => asset_detail_chassis,
-          :location_led_state => 'Blinking'
-        )
+      it "should migrate the data from physical_chassis and physical_servers to asset_details table" do
+        asset_detail_chassis = asset_detail_stub.create!(:resource_type => "PhysicalChassis", :resource_id => physical_chassis.id)
+        asset_detail_server  = asset_detail_stub.create!(:resource_type => "PhysicalServer", :resource_id => physical_server.id)
 
         migrate
 
-        expect(asset_detail_server.reload.location_led_state).to  eq('Off')
         expect(asset_detail_chassis.reload.location_led_state).to eq('Blinking')
+        expect(asset_detail_server.reload.location_led_state).to  eq('Off')
+      end
+
+      it "should create associated asset_detail and migrate the data if it doesn't exist" do
+        migrate
+
+        expect(asset_detail_stub.find_by(:resource_type => "PhysicalChassis", :resource_id => physical_chassis.id)).to have_attributes(:location_led_state => 'Blinking')
+        expect(asset_detail_stub.find_by(:resource_type => "PhysicalServer", :resource_id => physical_server.id)).to   have_attributes(:location_led_state => 'Off')
       end
     end
 
     migration_context :down do
+      let!(:physical_chassis) { physical_chassis_stub.create! }
+      let!(:physical_server)  { physical_server_stub.create! }
+
       it "should migrate the data back from asset_details to physical_chassis and physical_servers tables" do
-        asset_detail_server = asset_detail_stub.create!(
-          :location_led_state => 'Off'
-        )
-        server = physical_server_stub.create!(
-          :asset_detail => asset_detail_server
-        )
-
-        asset_detail_chassis = asset_detail_stub.create!(
-          :location_led_state => 'Blinking'
-        )
-        chassis = physical_chassis_stub.create!(
-          :asset_detail => asset_detail_chassis
-        )
+        asset_detail_chassis = asset_detail_stub.create!(:location_led_state => 'Blinking', :resource_type => "PhysicalChassis", :resource_id => physical_chassis.id)
+        asset_detail_server  = asset_detail_stub.create!(:location_led_state => 'Off', :resource_type => "PhysicalServer", :resource_id => physical_server.id)
 
         migrate
 
-        expect(server.reload.location_led_state).to  eq('Off')
-        expect(chassis.reload.location_led_state).to eq('Blinking')
+        expect(physical_chassis.reload.location_led_state).to eq('Blinking')
+        expect(physical_server.reload.location_led_state).to  eq('Off')
       end
-    end
-  end
 
-  context "resources without asset detail" do
-    migration_context :up do
-      it "should migrate the data from physical_chassis and physical_servers to asset_details table" do
-        server = physical_server_stub.create!(
-          :location_led_state => 'Off'
-        )
-
-        chassis = physical_chassis_stub.create!(
-          :location_led_state => 'Blinking'
-        )
-
-        migrate
-
-        expect(server.reload.asset_detail.location_led_state).to  eq('Off')
-        expect(chassis.reload.asset_detail.location_led_state).to eq('Blinking')
-      end
-    end
-
-    migration_context :down do
       it "should migrate the data back from asset_details to physical_chassis and physical_servers tables" do
-        server = physical_server_stub.create!
-
-        chassis = physical_chassis_stub.create!
-
         migrate
 
-        expect(server.reload.location_led_state).to  be_nil
-        expect(chassis.reload.location_led_state).to be_nil
+        expect(physical_chassis.reload.location_led_state).to be_nil
+        expect(physical_server.reload.location_led_state).to  be_nil
       end
     end
   end
